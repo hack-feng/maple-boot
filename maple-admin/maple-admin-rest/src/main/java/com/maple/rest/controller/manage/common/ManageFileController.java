1381dd205071eb4a372dbc97f45d6d70600f44b5
笑小枫 <1150640979@qq.com>
2024-06-04 16:59:29 +0800

    ✨ 添加图片上传本地功能

---
+++ maple-admin/maple-admin-rest/src/main/java/com/maple/rest/controller/manage/common/ManageFileController.java
@@ -0,0 +1,66 @@
+package com.maple.rest.controller.manage.common;
+
+import com.maple.common.config.exception.ErrorCode;
+import com.maple.common.config.exception.MapleCommonException;
+import com.maple.common.util.LocalFileUtil;
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 笑小枫-https://www.xiaoxiaofeng.com
+ */
+@RestController
+@RequestMapping("/manage/file")
+@Api(tags = "基础功能-文件上传下载")
+@AllArgsConstructor
+@Slf4j
+public class ManageFileController {
+
+    private final LocalFileUtil fileUtil;
+
+    /**
+     * 图片上传
+     */
+    @PostMapping("/uploadImage")
+    public String uploadImage(@RequestParam(value = "file") MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new MapleCommonException(ErrorCode.COMMON_ERROR, "图片内容为空，上传失败!");
+        }
+        return fileUtil.uploadImage(file);
+    }
+
+    /**
+     * 文件批量上传
+     */
+    @PostMapping("/uploadFiles")
+    public List<Map<String, Object>> uploadFiles(@RequestParam(value = "file") MultipartFile[] files) {
+        return fileUtil.uploadFiles(files);
+    }
+
+    /**
+     * 批量删除文件
+     */
+    @PostMapping("/deleteFiles")
+    public void deleteFiles(@RequestParam(value = "files") String[] files) {
+        fileUtil.deleteFile(files);
+    }
+
+    /**
+     * 文件下载功能
+     */
+    @GetMapping(value = "/download/{fileName:.*}")
+    public void download(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        try {
+            fileUtil.downLoadFile(response, fileName);
+        } catch (Exception e) {
+            log.error("文件下载失败", e);
+        }
+    }
+}
